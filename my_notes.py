e7ddd3b626d8d053800d74985ae684f9741fda11
gleb-erokhin <gleb.erokhin@gmail.com>
2023-07-28 06:44:58 +0300

    инициализация приложения - Записная книжка

---
+++ my_notes.py
@@ -0,0 +1,9 @@
+import csv
+# for csv file in VSC use plugin rainbowCSV
+# структура файла csv принята такой:
+# 2(порядковый номер записи),Текст записи,Дата записи
+
+
+
+if __name__ == '__main__':
+    main()
