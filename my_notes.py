89a300ffa22418945b016f0ef6541fef5fcede28
gleb-erokhin <gleb.erokhin@gmail.com>
2023-07-28 06:46:45 +0300

    создание функции Меню приложения - Записная книжка

--- my_notes.py
+++ my_notes.py
@@ -3,7 +3,27 @@ import csv
 # структура файла csv принята такой:
 # 2(порядковый номер записи),Текст записи,Дата записи
 
-
+def menu(list):
+    routine = True
+    while routine:
+        print("")
+        print("The main menu")
+        print("1 - Print all notes")
+        print("2 - Add note")
+        print("3 - Change note")
+        print("4 - Exit")
+        command = int(input("Choose number \n"))
+        print("")
+        if command == 1:  # повторная печать после изменений
+            data = read_data()
+            print_data(data)
+        elif command == 2:
+            add_data(list)  # для добавления записи
+        elif command == 3:
+            change_data(list)  # для изменения записи
+        elif command == 4:  # выход из программы
+            routine = False
+            break
 
 if __name__ == '__main__':
     main()
